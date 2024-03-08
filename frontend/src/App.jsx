786f197db94ce205327e5eb4257a576ed8826a8f
Ganesh Rana <ganeshrana6377772039@gmail.com>
2024-03-08 17:58:42 +0530

    created Header section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,11 +6,13 @@ import Signin from './pages/Signin.jsx'
 import Signup from './pages/Signup.jsx'
 import Dashboard from './pages/Dashboard.jsx'
 import Projects from './pages/Projects.jsx'
+import Header from './components/Header.jsx'
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/about' element={<About/>} />
