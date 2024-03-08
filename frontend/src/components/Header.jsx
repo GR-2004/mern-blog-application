913fee1bbd3f27d1b7a46219621b8edb92b10d48
Ganesh Rana <ganeshrana6377772039@gmail.com>
2024-03-08 18:31:48 +0530

    create and run the server

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
         to="/"
         className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
       >
-        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+        <span className="px-2 mx-1 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
           Blogify
         </span>
         Where Stories Unfold
@@ -33,7 +33,7 @@ const Header = () => {
           <FaMoon />
         </Button>
         <Link to='/sign-in'>
-          <Button gradientDuoTone='purpleToBlue'>
+          <Button gradientDuoTone='purpleToBlue' outline>
             Sign in
           </Button>
         </Link>
