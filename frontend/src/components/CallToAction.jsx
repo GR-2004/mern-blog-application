fe9d7869b143c5334216ecc3f4cfae5482b5ea57
Ganesh Rana <ganeshrana6377772039@gmail.com>
2024-07-12 16:43:26 +0530

    added ai-interview project

--- frontend/src/components/CallToAction.jsx
+++ frontend/src/components/CallToAction.jsx
@@ -3,18 +3,18 @@ import React from 'react'
 
 const CallToAction = () => {
   return (
-    <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
+    <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center bg-[#87CEEB]'>
       <div className="flex-1 flex flex-col justify-center">
-        <h2 className='text-2xl'>Want to learn more about JavaScript?</h2>
-        <p className='text-gray-500 my-2'>Checkout these resources with 100 JavaScript Projects</p>
+        <h2 className='text-3xl font-bold text-white'>Want to analyze your growth</h2>
+        <p className='text-gray-200 my-2 font-semibold'>Checkout these resources with </p>
         <Button gradientDuoTone={"purpleToPink"} className='rounded-tl-xl rounded-bl-none'>
-            <a href="https://www.100jsprojects.com" target='_blank' rel='noopener noreferrer'>
-                100 JavaScript Projects
+            <a href="https://ai-interview-one-pied.vercel.app/" target='_blank' rel='noopener noreferrer'>
+                AI Interview Mocker
             </a>
         </Button>
       </div>
       <div className="p-7 flex-1">
-        <img src="https://bairesdev.mo.cloudinary.net/blog/2023/08/What-Is-JavaScript-Used-For.jpg" />
+        <img src="/ai-interview-page.png" />
       </div>
     </div>
   )
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -6,7 +6,7 @@ import {BsFacebook, BsGithub, BsInstagram, BsLinkedin, BsTwitter} from 'react-ic
 const FooterCom = () => {
   return (
     <Footer container className="border border-t-8 border-teal-500">
-      <div className="w-full max-w-7xl mx-auto">
+      <div className="w-full max-w-3xl mx-auto">
         <div className="grid w-full justify-between sm:flex md:grid-cols-1">
           <div className="mt-5">
             <Link
@@ -23,13 +23,6 @@ const FooterCom = () => {
             <div>
               <Footer.Title title="About" />
               <Footer.LinkGroup col>
-                <Footer.Link
-                  href="https://www.100jsprojects.com"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  100 JS Projects
-                </Footer.Link>
                 <Footer.Link
                   href="/about"
                   target="_blank"
@@ -49,13 +42,11 @@ const FooterCom = () => {
                 >
                   Github
                 </Footer.Link>
-                <Footer.Link href="#">Discord</Footer.Link>
               </Footer.LinkGroup>
             </div>
             <div>
               <Footer.Title title="Legal" />
               <Footer.LinkGroup col>
-                <Footer.Link href="#">Privacy Policy</Footer.Link>
                 <Footer.Link href="#">Terms &amp; Conditions</Footer.Link>
               </Footer.LinkGroup>
             </div>
@@ -65,11 +56,8 @@ const FooterCom = () => {
         <div className="w-full sm:flex sm:items-center sm:justify-between">
             <Footer.Copyright href='#' by="Blogify" year={new Date().getFullYear()} />
             <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
-                <Footer.Icon href="#" icon={BsFacebook} />
-                <Footer.Icon href="#" icon={BsInstagram} />
-                <Footer.Icon href="#" icon={BsTwitter} />
                 <Footer.Icon href="https://github.com/GR-2004" icon={BsGithub} />
-                <Footer.Icon href="#" icon={BsLinkedin} />
+                <Footer.Icon href="https://www.linkedin.com/in/ganesh-rana/" icon={BsLinkedin} />
             </div>
         </div>
       </div>
